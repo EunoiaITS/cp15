7065e2a76e1acded0ada72a99511ef924f93c99e
Toki EIS <abdurrahman@eunoiaits.com>
2018-01-16 11:38:31 +0600

    Cotrollers and their functions
    Controller structure for user hierarchy and functionalities.

---
+++ app/Http/Controllers/AEMController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AEMController extends Controller
+{
+    public function addSupplier(Request $request){
+        //
+    }
+
+    public function editSupplier(Request $request){
+        //
+    }
+
+    public function addSupplierExcel(Request $request){
+        //
+    }
+
+    public function deleteSupplier(Request $request){
+        //
+    }
+
+    public function addQROrder(Request $request){
+        //
+    }
+
+    public function editQROrder(Request $request){
+        //
+    }
+
+    public function addQROrderExcel(Request $request){
+        //
+    }
+
+    public function deleteQROrder(Request $request){
+        //
+    }
+
+    public function inviteSuppliers(Request $request){
+        //
+    }
+
+    public function supplierQuotations(Request $request){
+        //
+    }
+
+    public function tenderSummery(Request $request){
+        //
+    }
+}
---
+++ app/Http/Controllers/DirectorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DirectorController extends Controller
+{
+    public function viewQR(Request $request){
+        //
+    }
+
+    public function viewSuppliers(Request $request){
+        //
+    }
+
+    public function approveQuotations(Request $request){
+        //
+    }
+
+    public function allowPriceShow(Request $request){
+        //
+    }
+}
