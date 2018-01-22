2e9e36ca2c3872953550dad8a4d83fecadfbd1a2
Mashuk-EIS <mushfiqur.rahman@eunoiaits.com>
2018-01-22 16:39:34 +0600

    add hash

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
@@ -35,14 +36,18 @@ class UsersController extends Controller
         }
         if($request->isMethod('post')){
             foreach ($user as $pass){
-            if($pass['password'] != bcrypt($request->old_pass)){
-                return redirect()->back()->withErrors("Old Password does not match");
-            }elseif($request->new_pass != $request->retype_pass){
-                return redirect()->back()->withErrors("New password does not match");
-            }else{
-                $user->password = bcrypt($request->new_pass);
-                $user->save();
-            }
+                if (!Hash::check('$request->old_pass', $pass['password'])) {
+                    return redirect()->back()->withErrors("Old Password does not match");
+                }
+                elseif($request->new_pass != $request->retype_pass){
+                    return redirect()->back()->withErrors("New password does not match");
+                }elseif(Hash::needsRehash($hashed)) {
+                    $hashed = Hash::make($request->new_pass);
+                    $user->password = $hashed;
+                    $user->save();
+                }else{
+                    return redirect()->back()->withErrors("Something went wrong");
+                }
             }
         }
         return view('users.change-password')->with('success-message', 'New Supplier added successfully!');;
