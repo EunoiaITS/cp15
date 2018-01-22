1e757a363a21a78fd7177ef789aafcff309c1b9f
Toki EIS <abdurrahman@eunoiaits.com>
2018-01-22 13:11:34 +0600

    login route

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+@section('content')
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-offset-1 col-md-10 col-sm-12">
+                    <div class="login-form dashboard-cs clearfix">
+                        <h3>Dashboard</h3>
+                        <div class="submenu-das clearfix">
+                            <a href="director-qr-order.html" class="btn btn-info btn-left odd">BBC Estate VII Sdn Bhd</a>
+                            <a href="qr-create-supplier.html" class="btn btn-info btn-left">BBC Pelita (Nyalau) Plantation Sdn Bhd</a>
+                            <a href="executive-create-supplier.html" class="btn btn-info btn-left odd">Almabumi (BUKTI) Plantation Sdn Bhd</a>
+                            <a href="admin-create-supplier.html" class="btn btn-info btn-left">BBC Bumihas Sdn Bhd</a>
+                            <a href="supplier-qr-order.html" class="btn btn-info btn-left odd">Almabumi (SUNGAI) Plantation Sdn Bhd</a>
+                            <button class="btn btn-info btn-left">BBC Palm Oil Mill Sdn Bhd</button>
+                            <button class="btn btn-info btn-left odd">BBC Pelita Plantation (Jepak) Sdn Bhd</button>
+                            <button class="btn btn-info btn-left">Majrany Plantation Sd Bhd</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'UsersController@dashboard');
 
 Route::get('/login', 'UsersController@login');
 Route::post('/login', 'UsersController@login');
