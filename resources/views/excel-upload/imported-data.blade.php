a0d5da17cdcb3aef098389661ce74467ba0777a3
Mashuk-EIS <mushfiqur.rahman@eunoiaits.com>
2018-01-29 17:18:21 +0600

    1 supplier fetched

--- resources/views/excel-upload/imported-data.blade.php
+++ resources/views/excel-upload/imported-data.blade.php
@@ -7,22 +7,27 @@
                     <div class="col-sm-10 padding-left-0">
                         <div class="create-qr qr-overfollow">
                             <h3 class="text-uppercase color-bbc">Excel Upload</h3>
+                            <style>
+                                table,tr,td{
+                                    border:1px solid black;
+                                }
+                            </style>
                             <table>
                                 <tr>
                                     <td>Name</td>
                                     <td>Email</td>
                                     <td>Category</td>
                                     <td>Contact</td>
+                                    <td>Password</td>
+
                                 </tr>
-                                <?php echo "<pre>"; print_r($results);echo "</pre>";?>
-
-                                 <tr>
-                                     <td></td>
-                                     <td>Email</td>
-                                     <td></td>
-                                     <td>Contact</td>
-                                    <tr>
-
+                                <tr>
+                                    <td><?php echo $results[0][0]['name'];?></td>
+                                    <td><?php echo $results[0][0]['email'];?></td>
+                                    <td><?php echo $results[0][0]['category'];?></td>
+                                    <td><?php echo $results[0][0]['contact'];?></td>
+                                    <td><?php echo $results[0][0]['password'];?></td>
+                                </tr>
                             </table>
                         </div>
                     </div>
