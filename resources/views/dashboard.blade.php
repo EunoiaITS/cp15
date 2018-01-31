1c324fe98429c5231c6ac29770ff5cb4c49b1945
Mashuk-EIS <mushfiqur.rahman@eunoiaits.com>
2018-01-31 14:27:45 +0600

    dashboard modified

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,14 +7,14 @@
                     <div class="login-form dashboard-cs clearfix">
                         <h3>Dashboard</h3>
                         <div class="submenu-das clearfix">
-                            <a href="director-qr-order.html" class="btn btn-info btn-left odd">BBC Estate VII Sdn Bhd</a>
-                            <a href="qr-create-supplier.html" class="btn btn-info btn-left">BBC Pelita (Nyalau) Plantation Sdn Bhd</a>
-                            <a href="executive-create-supplier.html" class="btn btn-info btn-left odd">Almabumi (BUKTI) Plantation Sdn Bhd</a>
-                            <a href="admin-create-supplier.html" class="btn btn-info btn-left">BBC Bumihas Sdn Bhd</a>
-                            <a href="supplier-qr-order.html" class="btn btn-info btn-left odd">Almabumi (SUNGAI) Plantation Sdn Bhd</a>
-                            <button class="btn btn-info btn-left">BBC Palm Oil Mill Sdn Bhd</button>
-                            <button class="btn btn-info btn-left odd">BBC Pelita Plantation (Jepak) Sdn Bhd</button>
-                            <button class="btn btn-info btn-left">Majrany Plantation Sd Bhd</button>
+                            <a href="" class="btn btn-info btn-left odd">BBC Estate VII Sdn Bhd</a>
+                            <a href="" class="btn btn-info btn-left">BBC Pelita (Nyalau) Plantation Sdn Bhd</a>
+                            <a href="" class="btn btn-info btn-left odd">Almabumi (BUKTI) Plantation Sdn Bhd</a>
+                            <a href="" class="btn btn-info btn-left">BBC Bumihas Sdn Bhd</a>
+                            <a href="" class="btn btn-info btn-left odd">Almabumi (SUNGAI) Plantation Sdn Bhd</a>
+                            <a href="" class="btn btn-info btn-left">BBC Palm Oil Mill Sdn Bhd</a>
+                            <a href="" class="btn btn-info btn-left odd">BBC Pelita Plantation (Jepak) Sdn Bhd</a>
+                            <a href="" class="btn btn-info btn-left">Majrany Plantation Sd Bhd</a>
                         </div>
                     </div>
                 </div>
