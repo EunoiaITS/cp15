511157d5642920c50847dbbff69f16cbc1db8840
Toki EIS <abdurrahman@eunoiaits.com>
2018-02-05 14:18:56 +0600

    no public extension fix

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,16 +1,16 @@
 </div>
 
 <!-- js file -->
-<script src="{{ URL::asset('assets/js/vendor/modernizr-3.5.0.min.js') }}"></script>
+<script src="{{ URL::asset('public/assets/js/vendor/modernizr-3.5.0.min.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="{{ URL::asset('assets/js/vendor/jquery-3.2.1.min.js') }}"><\/script>')</script>
-<script src="{{ URL::asset('assets/js/plugins.js') }}"></script>
-<script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
-<script src="{{ URL::asset('assets/js/jquery.nicescroll.min.js') }}"></script>
-<script src="{{ URL::asset('assets/js/moment.js') }}"></script>
-<script src="{{ URL::asset('assets/js/Chart.bundle.js') }}"></script>
-<script src="{{ URL::asset('assets/js/bootstrap-datetimepicker.js') }}"></script>
-<script src="{{ URL::asset('assets/js/main.js') }}"></script>
+<script>window.jQuery || document.write('<script src="{{ URL::asset('public/assets/js/vendor/jquery-3.2.1.min.js') }}"><\/script>')</script>
+<script src="{{ URL::asset('public/assets/js/plugins.js') }}"></script>
+<script src="{{ URL::asset('public/assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('public/assets/js/jquery.nicescroll.min.js') }}"></script>
+<script src="{{ URL::asset('public/assets/js/moment.js') }}"></script>
+<script src="{{ URL::asset('public/assets/js/Chart.bundle.js') }}"></script>
+<script src="{{ URL::asset('public/assets/js/bootstrap-datetimepicker.js') }}"></script>
+<script src="{{ URL::asset('public/assets/js/main.js') }}"></script>
 @if(isset($footer_js))
 @include($footer_js)
 @endif
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -8,13 +8,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="manifest" href="site.webmanifest">
-    <link rel="apple-touch-icon" href="{{ URL::asset('assets/icon.png') }}">
+    <link rel="apple-touch-icon" href="{{ URL::asset('public/assets/icon.png') }}">
     <!-- Place favicon.ico in the root directory -->
     <!-- font awsome css -->
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('public/assets/css/font-awesome.min.css') }}">
     <!-- css  file linking -->
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/normalize.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/main.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap-datetimepicker.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('public/assets/css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('public/assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('public/assets/css/main.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('public/assets/css/bootstrap-datetimepicker.css') }}">
 </head>
