8e0035c352ed3ca69e73cd7d500712fc9537ca2c
Mashuk-EIS <mushfiqur.rahman@eunoiaits.com>
2018-04-24 17:45:17 +0600

    QR Dynamic add items fixed

--- resources/views/qr_orders/qr-orders-js.blade.php
+++ resources/views/qr_orders/qr-orders-js.blade.php
@@ -20,15 +20,4 @@
             });
         });
     });
-
-    {{--var cat = 'input.category';--}}
-    {{--var data_no = [<?php echo $cat; ?>];--}}
-    {{--var options_no = {--}}
-        {{--source: data_no,--}}
-        {{--minLength: 0--}}
-    {{--};--}}
-    {{--var targetName = null;--}}
-    {{--$(document).on('keydown.autocomplete', cat, function() {--}}
-        {{--$(this).autocomplete(options_no);--}}
-    {{--});--}}
 </script>
