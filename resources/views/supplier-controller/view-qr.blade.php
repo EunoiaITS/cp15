8ff94f44b430b908cc569000ad11e0be9c992ee4
Mashuk-EIS <mushfiqurrahman.eis@gmail.com>
2018-11-18 11:16:59 +0600

    unit price fixed

--- resources/views/supplier-controller/view-qr.blade.php
+++ resources/views/supplier-controller/view-qr.blade.php
@@ -96,7 +96,7 @@
                                                     <th>OEM</th>
                                                     <th>Brand</th>
                                                     <th>Deliver Date</th>
-                                                    <th>Unit Price (including tax)</th>
+                                                    <th>Unit Price (including tax)(RM)</th>
                                                     <th>Comments</th>
                                                     <th>Upload File</th>
                                                 </tr>
@@ -108,7 +108,7 @@
                                                     <td><input type="text" name="oem0" class="form-control from-btn-supplier from-qr"></td>
                                                     <td><input type="text" name="brand0" class="form-control from-btn-supplier from-qr"></td>
                                                     <td><input type="text" name="delivery_date0" class="form-control from-qr from-supplier datepicker-f"></td>
-                                                    <td><input type="text" name="unit_price0" class="form-control from-btn-supplier from-qr unit-price" required></td>
+                                                    <td><input type="number" placeholder="0.0" name="unit_price0" class="form-control from-btn-supplier from-qr unit-price" required></td>
                                                     <td><input type="text" name="comment0" class="form-control from-qr from-supplier"><input id="total" type="hidden" name="count" value="0"></td>
                                                     <td>
                                                         <div class="file btn btn-sm btn-primary btn-supplier">
