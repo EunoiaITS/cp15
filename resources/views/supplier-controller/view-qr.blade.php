29a666e43bd8cb76f55f9792175ecc3e46b4ede4
Mashuk-EIS <mushfiqurrahman.eis@gmail.com>
2019-01-06 14:23:14 +0600

    Change

--- resources/views/supplier-controller/view-qr.blade.php
+++ resources/views/supplier-controller/view-qr.blade.php
@@ -43,8 +43,8 @@
                                                 <td>@if(isset($item->details->end_date)){{ date('d/m/Y',strtotime($item->details->end_date)) }}@endif</td>
                                                 <td><button type="button" class="btn btn-primary btn-supplier input-upload" data-toggle="modal" data-target="#prmodal{{$item->id}}" rel="{{ $item->id }}">Add Quotation</button></td>
                                             </tr>
-                                            @endif
                                         @endif
+                                    @endif
                                 @endforeach
                                 </tbody>
                             </table>
@@ -66,86 +66,86 @@
 
     <!-- place quotation -->
     @foreach($items as $item)
-                <form id="submit-qr{{ $item->id }}" action="{{ url('/supplier-controller/submit-qr/') }}" method="post" enctype="multipart/form-data">
-                    {{csrf_field()}}
-                    <div id="prmodal{{ $item->id }}" class="modal fade bs-example-modal-lg popup-prid-comparison" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-                        <div class="popup-base">
-                            <div class="search-popup">
-                                <i class="close fa fa-remove close" data-dismiss="modal" aria-label="Close"></i>
-                                <div class="row">
-                                    <div class="search-destination">
-                                        <h2 class="search-title">Place Quatation</h2>
-                                    </div>
-                                    <!-- header got seach area -->
-                                    <div class="popup-got-search">
-                                        <div class="table table-responsive">
-                                            <table class="table">
-                                                <thead>
-                                                <tr>
-                                                    <th>Origin</th>
-                                                    <th>Genuine</th>
-                                                    <th>OEM</th>
-                                                    <th>Brand</th>
-                                                    <th>Deliver Date</th>
-                                                    <th>Unit Price (including tax)(RM)</th>
-                                                    <th>Comments</th>
-                                                    <th>Upload File</th>
-                                                </tr>
-                                                </thead>
-                                                <tbody id="add-item-table-item{{ $item->id }}">
-                                                <tr>
-                                                    <td><input type="text" name="origin0" class="form-control from-btn-supplier from-qr"><input type="hidden" name="item_id0" value="{{$item->id}}"></td>
-                                                    <td><input type="text" name="genuine0" class="form-control from-btn-supplier from-qr"></td>
-                                                    <td><input type="text" name="oem0" class="form-control from-btn-supplier from-qr"></td>
-                                                    <td><input type="text" name="brand0" class="form-control from-btn-supplier from-qr"></td>
-                                                    <td><input type="text" name="delivery_date0" class="form-control from-qr from-supplier datepicker-f"></td>
-                                                    <td><input type="text" placeholder="0.0" name="unit_price0" class="form-control from-btn-supplier from-qr unit-price" data-toggle="tooltip" data-placement="bottom" title="Please insert numeric values only (Eg: 5 or 7.5)" required></td>
-                                                    <td><input type="text" name="comment0" class="form-control from-qr from-supplier"></td>
-                                                    <td>
-                                                        <input class="total" type="hidden" name="count" value="0">
-                                                        <div class="file btn btn-sm btn-primary btn-supplier">
-                                                            <div class="upload-icon"><i class="fa fa-cloud-upload" aria-hidden="true"></i></div><span>Upload</span>
-                                                            <input type="file" name="attachment0" class="input-upload">
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                        <div class="place-quatation-submit">
-                                            <button type="button" class="btn btn-info btn-view-table edit-qr add-item-create" rel="{{ $item->id }}">Add Item</button>
-                                            <button type="button" class="btn btn-info btn-view-table confirm" data-toggle="modal" data-target="#confirm{{ $item->id }}" rel="{{ $item->id }}">Submit</button>
-                                            <button type="button" class="btn btn-info btn-view-table  edit-qr">Cancel</button>
-                                        </div>
-                                    </div><!--// end header got search area -->
-                                </div>
+        <form id="submit-qr{{ $item->id }}" action="{{ url('/supplier-controller/submit-qr/') }}" method="post" enctype="multipart/form-data">
+            {{csrf_field()}}
+            <div id="prmodal{{ $item->id }}" class="modal fade bs-example-modal-lg popup-prid-comparison" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                <div class="popup-base">
+                    <div class="search-popup">
+                        <i class="close fa fa-remove close" data-dismiss="modal" aria-label="Close"></i>
+                        <div class="row">
+                            <div class="search-destination">
+                                <h2 class="search-title">Place Quatation</h2>
                             </div>
+                            <!-- header got seach area -->
+                            <div class="popup-got-search">
+                                <div class="table table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                        <tr>
+                                            <th>Origin</th>
+                                            <th>Genuine</th>
+                                            <th>OEM</th>
+                                            <th>Brand</th>
+                                            <th>Deliver Date</th>
+                                            <th>Unit Price (including tax)(RM)</th>
+                                            <th>Comments</th>
+                                            <th>Upload File</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody id="add-item-table-item{{ $item->id }}">
+                                        <tr>
+                                            <td><input type="text" name="origin0" class="form-control from-btn-supplier from-qr"><input type="hidden" name="item_id0" value="{{$item->id}}"></td>
+                                            <td><input type="text" name="genuine0" class="form-control from-btn-supplier from-qr"></td>
+                                            <td><input type="text" name="oem0" class="form-control from-btn-supplier from-qr"></td>
+                                            <td><input type="text" name="brand0" class="form-control from-btn-supplier from-qr"></td>
+                                            <td><input type="text" name="delivery_date0" class="form-control from-qr from-supplier datepicker-f"></td>
+                                            <td><input type="text" placeholder="0.0" name="unit_price0" class="form-control from-btn-supplier from-qr unit-price" data-toggle="tooltip" data-placement="bottom" title="Please insert numeric values only (Eg: 5 or 7.5)" required></td>
+                                            <td><input type="text" name="comment0" class="form-control from-qr from-supplier"></td>
+                                            <td>
+                                                <input class="total" type="hidden" name="count" value="0">
+                                                <div class="file btn btn-sm btn-primary btn-supplier">
+                                                    <div class="upload-icon"><i class="fa fa-cloud-upload" aria-hidden="true"></i></div><span>Upload</span>
+                                                    <input type="file" name="attachment0" class="input-upload">
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="place-quatation-submit">
+                                    <button type="button" class="btn btn-info btn-view-table edit-qr add-item-create" rel="{{ $item->id }}">Add Item</button>
+                                    <button type="button" class="btn btn-info btn-view-table confirm" data-toggle="modal" data-target="#confirm{{ $item->id }}" rel="{{ $item->id }}">Submit</button>
+                                    <button type="button" class="btn btn-info btn-view-table  edit-qr">Cancel</button>
+                                </div>
+                            </div><!--// end header got search area -->
                         </div>
                     </div>
-                </form>
+                </div>
+            </div>
+        </form>
     @endforeach
 
     @foreach($items as $item)
-    <!-- submit popup -->
-    <div id="confirm{{ $item->id }}" class="modal fade bs-example-modal-lg popup-wrapper-delete" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-        <div class="popup-base">
-            <div class="search-popup">
-                <i class="close fa fa-remove"></i>
-                <div class="row">
-                    <!-- header got seach area -->
-                    <div class="popup-got-search">
-                        <p>Are you sure you want to submit ?</p>
-                    </div><!--// end header got search area -->
+        <!-- submit popup -->
+        <div id="confirm{{ $item->id }}" class="modal fade bs-example-modal-lg popup-wrapper-delete" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+            <div class="popup-base">
+                <div class="search-popup">
+                    <i class="close fa fa-remove"></i>
+                    <div class="row">
+                        <!-- header got seach area -->
+                        <div class="popup-got-search">
+                            <p>Are you sure you want to submit ?</p>
+                        </div><!--// end header got search area -->
                         <div class="col-sm-12">
                             <div class="btn-button-group clearfix">
                                 <button type="submit" class="btn btn-info btn-price" form="submit-qr{{ $item->id }}">Yes</button>
                                 <button type="button" class="btn btn-info btn-popup close">No</button>
                             </div>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     @endforeach
 
 @endsection
